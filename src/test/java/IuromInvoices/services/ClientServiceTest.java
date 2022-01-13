c98f2cbd6b755fcb9792280f08f56a4ccd011fd0
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2022-01-13 23:44:17 +0200

    teste unitare pentru serviciul client

--- src/test/java/IuromInvoices/services/ClientServiceTest.java
+++ src/test/java/IuromInvoices/services/ClientServiceTest.java
@@ -3,6 +3,7 @@ package IuromInvoices.services;
 
 import IuromInvoices.dao.daoClient.ClientDataAccessService;
 import IuromInvoices.exception.ClientNotFoundException;
+import IuromInvoices.models.Client;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,5 +42,50 @@ public class ClientServiceTest {
         Assertions.assertEquals("Clientul nu a fost gasit!", exception.getMessage());
     }
 
+    @Test
+    @DisplayName("Id valid")
+    void getClientById() {
+        //arrange
+        UUID id = UUID.randomUUID();
+        String nume = "Test";
+        String cui = "ROtest";
+        String adresa = "Adresa Test";
+        String nrCont = "ROXXBRDE200SVTEST";
+        Client client  = new Client(id, nume, cui, adresa, nrCont);
 
+        Mockito.when(clientDao.selectClientById(Mockito.any()))
+                .thenReturn(Optional.of(client));
+
+        //act
+        Client getClientByid = clientService.getClientById(id);
+
+        //assert
+        Assertions.assertNotNull(getClientByid);
+        Assertions.assertEquals(nume, getClientByid.getNume());
+        Assertions.assertEquals(cui, getClientByid.getCui());
+        Assertions.assertEquals(adresa, getClientByid.getAdresa());
+        Assertions.assertEquals(nrCont, getClientByid.getNrCont());
+    }
+
+    @Test
+    @DisplayName("Id invalid")
+    void updateClient() {
+        //arrange
+        UUID idInvalid = UUID.randomUUID();
+        String nume = "Test";
+        String cui = "ROtest";
+        String adresa = "Adresa Test";
+        String nrCont = "ROXXBRDE200SVTEST";
+        Client client  = new Client(idInvalid, nume, cui, adresa, nrCont);
+
+        Mockito.when(clientDao.updateClientById(Mockito.any(), Mockito.any()))
+                .thenReturn(0);
+
+        //act
+        ClientNotFoundException exception = Assertions.assertThrows(ClientNotFoundException.class,
+                () -> clientService.updateClient(idInvalid, client));
+
+        //assert
+        Assertions.assertEquals("Clientul nu a fost gasit!", exception.getMessage());
+    }
 }
